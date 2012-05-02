e482646a09dc2f0bb0f989ef177967b45a4861f3
Alex Veenendaal <alex@opendoorgonorth.com>
2012-05-02 20:13:14 +0100

    fixed bugs. introduced a (server-side) canvas renderer.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports = {
+    mjs: require('./lib/mjs'),
+    Circle: require('./lib/circle'),
+    Polygon: require('./lib/polygon'),
+    Vertex: require('./lib/vertex'),
     Voronoi: require('./lib/voronoi'),
     Rectangle: require('./lib/rect'),
     Site: require('./lib/site'),
     SiteList: require('./lib/site_list'),
-    Edge: require('./lib/edge'),
-    mjs: require('./lib/mjs'),
-    Vertex: require('./lib/vertex'),
-    Polygon: require('./lib/polygon')
+    Edge: require('./lib/edge')
 }
